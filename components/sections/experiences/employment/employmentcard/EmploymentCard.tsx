d080994c691f99d54cdc4e28218ae8d34e064da2
Jason Liu <jasonliu215@gmail.com>
2024-03-01 01:36:38 +1100

    Updated employment

--- components/sections/experiences/employment/employmentcard/EmploymentCard.tsx
+++ components/sections/experiences/employment/employmentcard/EmploymentCard.tsx
@@ -7,12 +7,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
+import { useState } from "react";
 
 type employmentCardProps = {
   data: employmentData;
 };
 
 function EmploymentCard({ data }: employmentCardProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
     <article
       className={`cursor-pointer w-full gap-3 | ${styles.employmentCard}`}
@@ -22,10 +28,16 @@ function EmploymentCard({ data }: employmentCardProps) {
           expandIcon={<FontAwesomeIcon icon={faAngleDown} className={`p-0`} />}
           aria-controls="panel1-content"
           id="panel1-header"
-          className={`${styles.summaryWrapper}`}
+          className={`
+          ${styles.summaryWrapper} |
+          ${isExpanded ? styles.expanded : styles.notExpanded}
+          `}
+          onClick={toggleExpanded}
         >
           <div className={`${styles.summary}`}>
-            <p className={`${styles.company} | font-bold text-primary-a50`}>{data.company}</p>
+            <p className={`${styles.company} | font-bold text-primary-a50`}>
+              {data.company}
+            </p>
             <p className={`${styles.dates}`}>
               {data.startDate} - {data.endDate}
             </p>
