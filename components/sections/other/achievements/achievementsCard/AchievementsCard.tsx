3df48dc81c8fafcc24d27cab99770be4934fac6f
Jason Liu <jasonliu215@gmail.com>
2024-03-04 22:54:01 +1100

    Updated achievement card

--- components/sections/other/achievements/achievementsCard/AchievementsCard.tsx
+++ components/sections/other/achievements/achievementsCard/AchievementsCard.tsx
@@ -10,8 +10,10 @@ export default function AchievementsCard({data}:achievementsCardProps) {
             <h3 className={`mt-0 mb-1`}>
                 <h6>{data.name}</h6>
             </h3>
-            <div className={`italic mb-2`}>
-                <p>{data.company} - <strong><em>{data.year}</em></strong></p>
+            <div className={`italic mb-2 flex flex-col gap-0 md:flex-row md:gap-1`}>
+                <p>{data.company}</p>
+                <p className={`hidden md:block`}>-</p>
+                <p><strong>({data.year})</strong></p>
             </div>
             <div className={`${styles.item__description}`}>
                 <p>{data.description}</p>
