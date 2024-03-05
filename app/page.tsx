da3fb33261f5de04a1c0ba00d5ac0c432631392d
Jason Liu <jasonliu215@gmail.com>
2024-03-05 23:59:51 +1100

    Fixed some weird spacing

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
   return (
     <section className={`flex flex-col`}>
       <HeroBanner />
-      <Grid container spacing={2} className={`w-full`}>
+      <Grid container spacing={2} className={`${styles.content} | w-full m-0`}>
         <Grid
           xs={1}
           sm={1}
