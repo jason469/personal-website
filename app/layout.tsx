6225c32e7d396d6b606810aa207e22ca0f481aa2
Jason Liu <jasonliu215@gmail.com>
2024-03-08 12:47:47 +1100

    Added some head tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,16 +3,29 @@ import "@/styles/reset.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { Metadata } from "next";
-import { epilogue, journeyBegins, sourceSansThree } from "@/constants/fonts.constants";
+import {
+  epilogue,
+  journeyBegins,
+  sourceSansThree,
+} from "@/constants/fonts.constants";
 import NavigationBar from "@/components/layout/navbar/navigationbar";
 import Footer from "@/components/layout/footer/footer";
 
 export const metadata: Metadata = {
   title: "Jason Liu",
-  description: "Jason Liu's custom website to showcase his portfolio",
+  description:
+    "The portfolio for Jason Liu, a really passionate software developer over in Aussie. I specialise in app development across a variety of frameworks, and bring a great deal of energy and noise to a team",
   icons: {
     icon: "/logos/favicon.ico",
   },
+  openGraph: {
+    title: "Jason Liu",
+    description:
+      "The portfolio for Jason Liu, a really passionate software developer over in Aussie. I specialise in app development across a variety of frameworks, and bring a great deal of energy and noise to a team",
+    url: "https://www.jasonliudev.com",
+    type: "website",
+  },
+  applicationName: "Jason Liu",
 };
 
 export default function RootLayout({
@@ -30,11 +43,20 @@ export default function RootLayout({
             `}
     >
       <head>
-        <title>Jason Liu</title>
+        {/* Base tags */}
+        <meta name="robots" content="noindex" />
+        <meta name="author" content="Jason Liu"></meta>
+        <meta name="referrer" content="origin"></meta>
+        <meta name="color-scheme" content="normal"></meta>
+
+        {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/logos/logo.png"></link>
         <link rel="shortcut icon" href="/logos/logo.png" />
         <meta name="theme-color" content="#008080" />
+
+        {/* Open Graph */}
+        <meta property="og:image" content="/logos/logo.png" />
       </head>
       <body className="">
         <NavigationBar />
